68231edee64907fe853381a8870af6f7d3e3a1dd
Velomary Andriamasy <velomaryandria@gmail.com>
2023-10-03 21:45:55 +0300

    Frontend awards 2023

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible')
+    }
+    else{
+      entry.target.classList.remove('visible')
+    }
+  })
+})
+
+const hiddenElement = document.querySelectorAll('.hidden')
+hiddenElement.forEach((el)=>observer.observe(el))
